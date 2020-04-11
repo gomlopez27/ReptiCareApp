173ad4d56fe20b001d16ed5a5175f960c714379d
Giovanna Lopez <g.lopez@campus.fct.unl.pt>
2020-04-11 17:20:33 +0100

    pedido login

--- app/src/main/java/com/example/repticare/LoginActivity.java
+++ app/src/main/java/com/example/repticare/LoginActivity.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class LoginActivity extends AppCompatActivity {
@@ -28,60 +32,87 @@ public class LoginActivity extends AppCompatActivity {
         mUsername = findViewById(R.id.username);
         mPassword = findViewById(R.id.password);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        url = getString(R.string.SERVER_URL);
+
 
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                // Reset errors.
-                mUsername.setError(null);
-                mPassword.setError(null);
-
-                String username = mUsername.getText().toString();
-                String password = mPassword.getText().toString();
-
-                boolean cancel = false;
-
-
-                // Check for a valid password, if the user entered one.
-                if (TextUtils.isEmpty(username)) {
-                    mUsername.setError("É necessario colocar um username.");
-                    cancel = true;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    mPassword.setError("É necessario colocar uma password.");
-                    cancel = true;
-                }
-
-
-                if(cancel == false) {
-
-                        if(user.getUser().equals(username)){
-                            if(user.getPassword().equals(password)) {
-
-
-
-                                cancel=true;
-                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                mPassword.setError("Password errada"); //private TextInputLayout textInputPwd;
-                                mPassword.requestFocus();
-
-                            }
-                        }
-                    }
-                    if(cancel==false){
-                        mPassword.setError("Password errada"); //private TextInputLayout textInputPwd;
-                        mPassword.requestFocus();
-                    }
-
+                attemptLogin();
                 }
 
         });
+
+    }
+
+    private void attemptLogin() {
+
+
+        // Reset errors.
+        mUsername.setError(null);
+        mPassword.setError(null);
+
+        String username = mUsername.getText().toString();
+        String password = mPassword.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPassword.setError("É necessario colocar uma password.");
+            focusView = mPassword;
+            cancel = true;
+        }
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(username)) {
+            mUsername.setError("É necessario colocar um username.");
+            focusView = mUsername;
+            cancel = true;
+        }
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        }
+
+        else {
+          //pedido REST LOGIN
+
+            RequestQueue queue = Volley.newRequestQueue(this);
+            url = getString(R.string.SERVER_URL);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            //textView.setText("Response is: "+ response.substring(0,500));
+
+                            if(response.equals("200")){
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                mPassword.setError("Password errada"); //private TextInputLayout textInputPwd;
+                                mPassword.requestFocus();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    //textView.setText("That didn't work!");
+                    mPassword.setError("Password errada"); //private TextInputLayout textInputPwd;
+                    mPassword.requestFocus();
+                }
+            });
+            queue.add(stringRequest);
+        }
+
+
+
+
+
 
     }
 }
