22d36605ae43372b14613da28290d4d8cfc53452
AndreOA <a.atalaia@campus.fct.unl.pt>
2020-05-07 18:31:32 +0100

    Notifications activaty and xml done

--- app/src/main/java/com/example/repticare/NotificationSettingsActivity.java
+++ app/src/main/java/com/example/repticare/NotificationSettingsActivity.java
@@ -1,14 +1,57 @@
 package com.example.repticare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+
+import androidx.appcompat.widget.SwitchCompat;
 
 public class NotificationSettingsActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_settings);
+
+        toolbar = findViewById(R.id.toolbar_notifications_settings);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_dark_green));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationSettingsActivity.this, AccountActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        SwitchCompat issuesNotifications = findViewById(R.id.issues_notifications);
+        SwitchCompat foodNotifications = findViewById(R.id.food_calendar_notifications);
+
+
+        issuesNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.e("issues" , "Boolean value = " + b);
+            }
+        });
+
+
+        foodNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.e("food" , "Boolean value = " + b);
+            }
+        });
+
     }
+
 }
+
