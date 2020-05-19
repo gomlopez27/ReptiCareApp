78d621af7f733a5fc72514f720f7dc4da2b6e5b2
Ema Vieira <er.vieira@campus.fct.unl.pt>
2020-05-19 15:32:07 +0100

    Implemented issue and revolve issue

--- app/src/main/java/com/example/repticare/IssueActivity.java
+++ app/src/main/java/com/example/repticare/IssueActivity.java
@@ -2,20 +2,36 @@ package com.example.repticare;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class IssueActivity extends AppCompatActivity {
+    Button button_resolve_issue;
+    EditText text_issue_description;
+    String url;
     Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO: buscar texto de descricao e fazer display
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue);
 
-        String issueName = getIntent().getExtras().getString("Name");
+        final String issueName = getIntent().getExtras().getString("Name");
         toolbar = findViewById(R.id.toolbar_issue);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(issueName);
@@ -29,5 +45,52 @@ public class IssueActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        button_resolve_issue = findViewById(R.id.button_resolve_issue);
+        button_resolve_issue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptResolveIssue();
+            }
+        });
     }
+
+    private void attemptResolveIssue() {
+
+        //pedido RESOLVE ISSUE
+
+        final String issueName = getIntent().getExtras().getString("Name");
+
+        url = getString(R.string.SERVER_URL_ANDRE) + "resolveIssue/";
+
+        JSONObject issue = new JSONObject();
+        try {
+            issue.put("issue_name", issueName);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.e("kk", issue.toString());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, issue, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //textView.setText("Response: " + response.toString());
+                        Intent intent = new Intent(IssueActivity.this, ListIssuesActivity.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("kk",error.toString());
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
 }
