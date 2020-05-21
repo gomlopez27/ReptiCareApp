2617568eee3fe92ee9dcd7c23d35cf8f100e0412
Giovanna Lopez <g.lopez@campus.fct.unl.pt>
2020-05-21 16:26:35 +0100

    stuff

--- app/src/main/java/com/example/repticare/AddUserActivity.java
+++ app/src/main/java/com/example/repticare/AddUserActivity.java
@@ -1,18 +1,35 @@
 package com.example.repticare;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddUserActivity extends AppCompatActivity {
     Toolbar toolbar;
     EditText username_et;
     Button addOtherUserButton;
+    private static final String COOKIE_KEY = "Cookie";
+    private static final String SESSION_COOKIE = "sessionid";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +37,7 @@ public class AddUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_user);
 
         final String terrariumName = getIntent().getExtras().getString("terrarium_name");
+        //TODO apaanhar o ID do terrarium
 
         toolbar = findViewById(R.id.toolbar_add_other_user);
         setSupportActionBar(toolbar);
@@ -36,5 +54,72 @@ public class AddUserActivity extends AppCompatActivity {
 
         username_et = findViewById(R.id.username_to_add);
         addOtherUserButton = findViewById(R.id.add_user_button);
+
+        addOtherUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptAddOtherUser();
+            }
+        });
+    }
+
+    private void attemptAddOtherUser(){
+        String url = getString(R.string.SERVER_URL_ANDRE) + "terrariums/addOtherUser/";
+
+        JSONObject otherUser = new JSONObject();
+        try {
+            otherUser.put("name", "");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, otherUser, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Intent intent = new Intent(AddUserActivity.this, ListTerrariumsActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("AddUser",error.getMessage());
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                addSessionCookie(params);
+                return params;
+            }
+
+        };
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    /**
+     * Adds session cookie to headers if exists.
+     * @param headers
+     */
+    private final void addSessionCookie(Map<String, String> headers) {
+        SharedPreferences settings = getSharedPreferences("Auth", 0);
+        String sessionId = settings.getString(SESSION_COOKIE, "");
+        if (sessionId.length() > 0) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(SESSION_COOKIE);
+            builder.append("=");
+            builder.append(sessionId);
+            if (headers.containsKey(COOKIE_KEY)) {
+                builder.append("; ");
+                builder.append(headers.get(COOKIE_KEY));
+            }
+            headers.put(COOKIE_KEY, builder.toString());
+        }
     }
 }
